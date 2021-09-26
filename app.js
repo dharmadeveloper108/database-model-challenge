881ba1105d24988189651d00cfa9a951247275d4
dharmadeveloper108 <whateverland108@gmail.com>
2021-09-26 16:59:58 +0200

    Fixes and refactors sortTables

--- app.js
+++ app.js
@@ -5,21 +5,21 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.listen(3000, '0.0.0.0', () => {
-    console.log("Server listening on port 3000");
-    readJson("./database.json");
+    console.log('Server listening on port 3000');
+    readJson('./database.json');
 });
 
 function readJson(jsonData) {
-    fs.readFile(jsonData, "utf8", (err, jsonString) => {
+    fs.readFile(jsonData, 'utf8', (err, jsonString) => {
         if (err) {
-          console.log("Error reading file: ", err);
+          console.log('Error reading file: ', err);
           return;
         }
     
         try {
             sortTables(jsonString);
         } catch (error) {
-            console.log("Error processing data: ", error);
+            console.log('Error processing data: ', error);
         }
     });      
 }
@@ -27,39 +27,35 @@ function readJson(jsonData) {
 function sortTables(data) {
     const tables = JSON.parse(data);
 
-    let foreignKeys = [];
     const prioritized = tables.map((t) => {
 
-        t.columns.forEach(col => {
-            if(col.foreign_key !== null) {
-                foreignKeys.push(col.foreign_key.split('.')[0]);
-            }
-        });
-
         return {
-            priority: 0, 
+            priority: 1, 
             name: t.name,
             columns: t.columns}
     });
 
     prioritized.forEach(table => {
-        if(foreignKeys.includes(table.name)) {
-            table.priority ++;
-        }
-
+     
         let noForeignKey = true;
         table.columns.forEach(col => {
             if(col.foreign_key !== null) {
+                let foreignTable = prioritized.find(p => p.name === col.foreign_key.split('.')[0]);
+                if(foreignTable.priority < prioritized.length)
+                    foreignTable.priority = table.priority +1;
                 noForeignKey = false;
             }
         });
 
-        if(noForeignKey) table.priority ++;
+        if(noForeignKey) {
+            table.priority = prioritized.length;
+        }
     });
 
     prioritized.sort((a, b) => b.priority - a.priority);
     const result = prioritized.map(p => p.name);
 
+    console.log(prioritized);
     console.log(result);
     return JSON.stringify(result);
 }
